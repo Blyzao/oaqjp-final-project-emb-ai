df5aae38bd4651ec0ce604ad0839afded21b461c
blyzao <blyzao@gmail.com>
2026-09-16 15:55:03 +0000

    Add docstrings to unit tests for pylint compliance

--- test_emotion_detection.py
+++ test_emotion_detection.py
@@ -9,22 +9,27 @@ class TestEmotionDetector(unittest.TestCase):
     """Check the dominant emotion returned for a set of sample statements."""
 
     def test_joy(self):
+        """The joy statement should be dominated by joy."""
         result = emotion_detector('I am glad this happened')
         self.assertEqual(result['dominant_emotion'], 'joy')
 
     def test_anger(self):
+        """The anger statement should be dominated by anger."""
         result = emotion_detector('I am really mad about this')
         self.assertEqual(result['dominant_emotion'], 'anger')
 
     def test_disgust(self):
+        """The disgust statement should be dominated by disgust."""
         result = emotion_detector('I feel disgusted just hearing about this')
         self.assertEqual(result['dominant_emotion'], 'disgust')
 
     def test_sadness(self):
+        """The sadness statement should be dominated by sadness."""
         result = emotion_detector('I am so sad about this')
         self.assertEqual(result['dominant_emotion'], 'sadness')
 
     def test_fear(self):
+        """The fear statement should be dominated by fear."""
         result = emotion_detector('I am really afraid that this will happen')
         self.assertEqual(result['dominant_emotion'], 'fear')
 
